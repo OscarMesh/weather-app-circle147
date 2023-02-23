ab8ba9a23e99b5838a73bd4698386e087b146500
osacrMesh <okerekeifeanyi65@gmail.com>
2023-02-23 18:38:40 +0100

    first-commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <main class="flex flex-col justify-center p-3 ">
+    <h1 class="text-lime-900 font-bold">Hi Guys! This is the HomeView!</h1>
+    <p>Let's Bulid a weather app! 😃</p>
+  </main>
+</template>
